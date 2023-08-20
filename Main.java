7644bd068d54476ca25aaa9b046f8f4d942b9230
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2023-08-20 23:48:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,94 +5,91 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        // Определен массив
+        // Source array
         int[] sourceArray = {70, 250, 50, 300, 1};
         System.out.println("Исходный массив: ");
-        // Выводим сидержимое исходного массива
+        // Output the contents of the source array
         System.out.println(Arrays.toString(sourceArray));
-        // Получаем минимальное и максимальное значения в массиве
+        // Getting the minimum and maximum values in the array
         Map<String, Integer> result = getMinMax(sourceArray);
-        // Отображаем мнинимальное и максимальное значения
+        // Displaying the minimum and maximum values
         System.out.println("Min = " + result.get("min"));
         System.out.println("Max = " + result.get("max"));
     }
 
     public static Map<String, Integer> getMinMax(int[] sourceArray) {
-        // Определяем переменную-контейнер для возврата результата
+        // Defining a container variable to return the result
         Map<String, Integer> results = new HashMap<>();
-        // Сортируем массив методом быстрой сортировки с использованием рекурсии
+        // Sorting the array using the fast sorting method using recursion
         quickSort(sourceArray, 0, sourceArray.length - 1);
-        // Помещаем минимальное значение в контейнер
+        // Putting the minimum value in the container
         results.put("min", sourceArray[0]);
-        // Помещаем максимальное значение в контейнер
+        // Putting the maximum value in the container
         results.put("max", sourceArray[sourceArray.length - 1]);
-        // возвращаем результат
+        // returning the result
         return results;
     }
 
     public static void quickSort(int[] array, int lowIndex, int highIndex) {
-        // Если массив пустой, то выходим
+        // If the array is empty, then exit
         if (array.length == 0)
             return;
 
-        // Нижний индекс должен быть меньше верхнего, если равен или больше, тогда выходим
+        // The lower index should be less than the upper one, if it is equal to or greater, then we exit
         if (lowIndex >= highIndex)
             return;
 
-        // Определяем индекс опорного элемента
+        // Defining the index of the reference element
         int middleIndex = lowIndex + (highIndex - lowIndex) / 2;
-        // Значение опорного элемента
+        // Value of the reference element
         int middleValue = array[middleIndex];
 
-        // Устанавливаем начальные значения индексов для проверки элементов справа и слева от опорного элемента
+        // Setting the initial values of the indexes to check the elements to the right and left of the reference element
         int lowIndexCheck = lowIndex, highIndexCheck = highIndex;
-        // Продолжаем перебор массива пока проверяемый нижний индекс меньше или равен проверяемому верхнему индексу
-        // Выходим из цикла, если проверяемый нижний индекс больше проверяемого верхнего
+        // We continue iterating through the array until the checked subscript is less than or equal to the checked superscript
+        // Exit the loop if the lower index being checked is greater than the upper index being checked
         while (lowIndexCheck <= highIndexCheck) {
-            // С левой стороны от опорного элемента (последовательным перебором индекса: от нуля в большую сторону)
-            //  находим элемент (и его индекс), который равен опорному элементу или больше него (движение слева направо)
+            // On the left side of the reference element (by sequentially iterating the index: from zero up), 
+            // we find an element (and its index) that is equal to or greater than the reference element (movement from left to right)
             while (middleValue > array[lowIndexCheck]) {
-                // Увеличиваем значение индекса, чтобы проверить следующий за ним элемент
-                // (движемся вправо - приближаемся к опорному элементу)
+                // Increasing the index value to check the next element (moving to the right - approaching the reference element)
                 lowIndexCheck++;
             }
-            // С правой стороны от опорного ищем элемент /**/элемент который равен опорному или меньше него
-            // (движение справо налево: значение индекса уменьшается от длина массива -1 ),
+            // On the right side of the reference element, we are looking for an element that is equal to or less than the reference element 
+            // (movement from right to left: the index value decreases from the length of the array -1 )
             while (array[highIndexCheck] > middleValue) {
-                // Увеличиваем значение индекса, чтобы проверить следующий за ним элемент
-                // (движемся влево - приближаемся к опорному элементу)
+                // Increasing the index value to check the next element (moving to the left - approaching the reference element)
                 highIndexCheck--;
             }
 
-            // Если нижний проверяемый индекс меньше верхнего проверяемого индекса,
-            // то меняем позиции этих элементов местами (взаимно меняем их индексы между ними)
+            // If the lower checked index is smaller than the upper checked index, 
+            // then we swap the positions of these elements (mutually change their indexes between them)
             if (lowIndexCheck <= highIndexCheck) {
-                if (array[lowIndexCheck] != array[highIndexCheck] && // Переставляем только элементы с разными значениями (могут быть и одинковые)!
-                    lowIndexCheck != highIndexCheck ) { // Переставлять одно и то же значение - тоже бесполезно обрабатывать (чтобы не переставлять одно и тот же элемент из своей позиции в свою же)!
-                    // сохраняем значение элемента из "левой" части массива (с "нижним" индексом) во временной переменной
+                if (array[lowIndexCheck] != array[highIndexCheck] && // We rearrange only elements with different values (there may be the same ones)!
+                    lowIndexCheck != highIndexCheck ) {  // Rearranging the same value is also useless to process (so as not to rearrange the same element from its position to its own)!
+                    // we store the value of the element from the "left" part of the array (with the "lower" index) in a temporary variable
                     int temp = array[lowIndexCheck];
-                    // присваиваем эначению с "нижним" индексом значение с "верхним" индексом (из "правой" части массива)
+                    // assign a value with a "lower" index to a value with an "upper" index (from the "right" part of the array)
                     array[lowIndexCheck] = array[highIndexCheck];
-                    // присваиваем значению с "верхним" индексом значение с "нижним" индексом (из временной переменной)
+                    // assign a value with an "upper" index to a value with a "lower" index (from a temporary variable)
                     array[highIndexCheck] = temp;
                 }
-                // Даже если элементы массива и не переставлялись - необходимо изменить значения следующих переменных (индексов)
-                // Переходим к элементу со следующим индексом (движемся вправо)
+                // Even if the array elements were not rearranged, it is necessary to change the values of the following variables (indexes)
+                // Go to the element with the following index (move to the right)
                 lowIndexCheck++;
-                // Переходим к элементу со следующим индексом (движемся влево)
+                // Go to the element with the following index (move to the left)
                 highIndexCheck--;
             }
         }
 
-        // вызовы рекурсии для сортировки левой и правой части
-        // Если нижний индекс меньше верхнего проверяемого индекс,
-        // то продолжаем сортировку и устанавливаем новые границы:
-        // Если нижний индекс равен верхнему проверяемому, то делать нечего
+        // recursion calls to sort the left and right parts
+        // If the lower index is less than the upper index being checked, then we continue sorting and set new boundaries:
+        // If the lower index is equal to the upper one being checked, then there is nothing to do
         if (lowIndex < highIndexCheck) {
             quickSort(array, lowIndex, highIndexCheck);
         }
 
-        // Если нижний проверяемый индекс меньше правой границы
+        // If the lower checked index is less than the right border
         if (lowIndexCheck < highIndex) {
             quickSort(array, lowIndexCheck, highIndex);
         }
